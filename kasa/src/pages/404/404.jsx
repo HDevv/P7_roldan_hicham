e84e014b133c487d7d363d64c73ccf03f38c674e
Hicham Roldan <82762193+Hicham017@users.noreply.github.com>
2022-11-16 16:02:16 +0100

    Routing et fichiers
    mise en place du routage et organisation des fichiers

---
+++ kasa/src/pages/404/404.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const err = () => {
+  return <div></div>;
+};
+
+export default err;
---
+++ kasa/src/pages/a-propos/a_propos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const a_propos = () => {
+  return <div></div>;
+};
+
+export default a_propos;
