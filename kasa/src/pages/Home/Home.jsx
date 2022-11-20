bfc2e73aaa8168de52f8dd8f4c5fb9f16d14318f
Hicham Roldan <82762193+Hicham017@users.noreply.github.com>
2022-11-20 21:04:01 +0100

    accordeons
    mise en place des composants accordéons

--- kasa/src/pages/Home/Home.jsx
+++ kasa/src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
+import Equipements from "../../components/equipements/equipements.jsx";
 
 const Home = () => {
   return (
     <div className="Home">
       {/* <Header /> */}
-      <Footer />
+      {/* <Footer /> */}
+      <div className="accordions">
+        <Equipements title="Titre 1" content="contenu" />
+      </div>
     </div>
   );
 };
