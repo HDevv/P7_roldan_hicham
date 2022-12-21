3a4efe91d168e1b606d6f9f6b0865d9e868bfcd4
Hicham Roldan <82762193+Hicham017@users.noreply.github.com>
2022-12-21 11:19:23 +0100

    composant cards fini
    problème de data reglé et intégration du CSS

--- kasa/src/components/cards/cards.jsx
+++ kasa/src/components/cards/cards.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Data from "../../data.json";
+import Data from "../../../data.json";
 import "../cards/cards.css";
 
 function Card() {
