0968fd4fa1d4b792506ba8f0ef1715397516e044
Hicham Roldan <82762193+Hicham017@users.noreply.github.com>
2023-01-02 16:25:28 +0100

    espacement entre les accordéons de about résolu

--- kasa/src/components/accordeons2/accordeons2.jsx
+++ kasa/src/components/accordeons2/accordeons2.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import "../accordeons1/description.css";
+import "../accordeons2/accordeons2.css";
 
 function Equipements({ title, content }) {
   const [active, setActive] = useState(false);
@@ -9,13 +9,11 @@ function Equipements({ title, content }) {
     setActive(!active);
   };
   return (
-    <div className="accordions_container">
-      <div className={`accordion ${active && "active"}`}>
-        <div className="accordion__title" onClick={handleToggle}>
-          {title} <span className="accordion__icon"></span>
-        </div>
-        <div className="accordion__content">{content}</div>
+    <div className={`accordion ${active && "active"}`}>
+      <div className="accordion__title" onClick={handleToggle}>
+        {title} <span className="accordion__icon"></span>
       </div>
+      <div className="accordion__content">{content}</div>
     </div>
   );
 }
--- kasa/src/pages/about/about.jsx
+++ kasa/src/pages/about/about.jsx
@@ -9,33 +9,41 @@ const about = () => {
     <div className="container">
       <Banner src={BgAbout} alt="Paysage montagnard" />
       <div className="accordions">
-        <Accordion2
-          title="Fiabilité"
-          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont
+        <div className="acc1">
+          <Accordion2
+            title="Fiabilité"
+            content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont
               conformes aux logements, et toutes les informations sont régulièrement vérifiées
               par nos équipes"
-        />
-        <Accordion2
-          title="Respect"
-          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
+          />
+        </div>
+        <div className="acc2">
+          <Accordion2
+            title="Respect"
+            content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
               discriminatoire ou de perturbation du voisinage entraînera une exclusion de
               notre plateforme."
-        />
+          />
+        </div>
 
-        <Accordion2
-          title="Service"
-          content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience
+        <div className="acc3">
+          <Accordion2
+            title="Service"
+            content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience
               parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.."
-        />
+          />
+        </div>
 
-        <Accordion2
-          title="Sécurité"
-          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les
+        <div className="acc4">
+          <Accordion2
+            title="Sécurité"
+            content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les
               voyageurs, chaque logement correspond aux critères de sécurité établis par nos
               services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à
               nos équipes de vérifier que les standards sont bien respectés. Nous organisons
               également des ateliers sur la sécurité domestique pour nos hôtes."
-        />
+          />
+        </div>
       </div>
     </div>
   );
