3de07112cf4d0d39854dc3c493b123211aed48f4
Hicham Roldan <82762193+Hicham017@users.noreply.github.com>
2023-05-07 19:49:42 +0200

    diapo, page logements et repositionnement des elements de la page about

--- kasa/src/components/accordion2/accordion2.jsx
+++ kasa/src/components/accordion2/accordion2.jsx
@@ -8,23 +8,27 @@ function Accordion2({ title, content }) {
   const [isActive, setIsActive] = useState(false);
 
   return isActive ? (
-    <div className="accordeon2">
-      <div className="accordeon2_header" onClick={() => setIsActive(false)}>
-        <h3>{title}</h3>
-        <img src={chevron} className="icone_ouverte" alt="arrow open" />
-      </div>
-      <div className="accordeon2_content">
-        <ul aria-hidden="false">{content}</ul>
+    <div className="acc_cont">
+      <div className="accordeon2">
+        <div className="accordeon2_header" onClick={() => setIsActive(false)}>
+          <h3>{title}</h3>
+          <img src={chevron} className="icone2_ouverte" alt="arrow open" />
+        </div>
+        <div className="accordeon2_content">
+          <ul aria-hidden="false">{content}</ul>
+        </div>
       </div>
     </div>
   ) : (
-    <div className="accordeon2">
-      <div className="accordeon2_header" onClick={() => setIsActive(true)}>
-        <h3>{title}</h3>
-        <img src={chevron} className="icone" alt="arrow closed" />
-      </div>
-      <div className="accordeon2_content_hidden" aria-hidden="true">
-        <ul aria-hidden="true">{content}</ul>
+    <div className="acc_cont">
+      <div className="accordeon2">
+        <div className="accordeon2_header" onClick={() => setIsActive(true)}>
+          <h3>{title}</h3>
+          <img src={chevron} className="icone2" alt="arrow closed" />
+        </div>
+        <div className="accordeon2_content_hidden" aria-hidden="true">
+          <ul aria-hidden="true">{content}</ul>
+        </div>
       </div>
     </div>
   );
--- kasa/src/components/footer/footer.jsx
+++ kasa/src/components/footer/footer.jsx
@@ -4,27 +4,8 @@ import "../footer/footer.css";
 import footerLogo from "../../assets/logo-footer.png";
 
 const footer = () => {
-  const [isFooterVisible, setFooterVisibility] = useState(true);
-
-  const handleScroll = () => {
-    const windowHeight = window.innerHeight;
-    const documentHeight = document.documentElement.scrollHeight;
-    const scrollTop = document.documentElement.scrollTop;
-    const isBottomOfPage = scrollTop + windowHeight >= documentHeight;
-
-    if (!isBottomOfPage) {
-      setFooterVisibility(true);
-    } else {
-      setFooterVisibility(false);
-    }
-  };
-
-  window.addEventListener("scroll", handleScroll);
   return (
-    <footer
-      className="footer"
-      style={{ display: isFooterVisible ? "flex" : "none" }}
-    >
+    <footer className="footer">
       <img src={footerLogo} alt="Logo du footer" className="footer_logo" />
 
       <p className="txt">© 2020 Kasa. All rights reserved</p>
